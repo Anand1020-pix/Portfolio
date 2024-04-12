e8d0cc262d71167d82d020d88902facb06fccf58
Anand1020-pix <59107989+Anand1020-pix@users.noreply.github.com>
2024-04-12 17:59:18 +0530

    01

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,19 +5,19 @@ import linkedin from "../../Assets/linkedin.png";
 
 function Contact() {
   return (
-<div class="  text-center block w-full  p-10 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700" >
+<div class="  text-center block w-full h-full p-10 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700" >
     
 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white  pb-4">Contact Me</h5>
 <div className="mr-10 flex gap-10 justify-center"> 
  
 <a href="mailto: anandmadhavan10201@gmail.com">
-  <img src={gmail} alt="Gmail" style={{ width: '70px', height: '70px' }} />
+  <img src={gmail} alt="Gmail" style={{ width: '60px', height: '60px' }} />
 </a>  
 <a href="https://github.com/Anand1020-pix" >
-<img src={github} alt="GitHub" style={{ width: '70px', height: '70px' }}/>
+<img src={github} alt="GitHub" style={{ width: '60px', height: '60px' }}/>
 </a>   
 <a href="https://www.linkedin.com/in/anand-m-202b651b3/">
-    <img src={linkedin} alt="LinkedIn"style={{ width: '70px', height: '70px' }} />
+    <img src={linkedin} alt="LinkedIn"style={{ width: '70px', height: '60px' }} />
     </a>
     </div>
      </div>
